e57b299fd87d0ead616cf586071a0dccd3ac6c6f
long4444 <453582280@qq.com>
2017-11-08 11:54:00 +0800

    修改

---
+++ mqxxgl/src/main/java/com/hotsun/mqxxgl/busi/activity/AddLdActivity.java
@@ -0,0 +1,26 @@
+package com.hotsun.mqxxgl.busi.activity;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hotsun.mqxxgl.R;
+
+
+public class AddLdActivity extends AppCompatActivity{
+
+    private Context mContext;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addld);
+
+//        mContext = AddLdActivity.this;
+//
+    }
+
+
+}
